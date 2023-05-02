a04fe2d7d5129e285e6fa6d503f952b23cff58f5
wmendozak <wmendozak@gmail.com>
2023-05-01 21:23:14 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAgeApp.Models;
+
+namespace WebAgeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index(DateTime? Birthdate)
+        {
+            Ages ages = new Ages();
+            ages.Birthdate = Birthdate;
+            return View(ages);
+        }
+
+        //    public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Ages.cs
@@ -0,0 +1,48 @@
+﻿namespace WebAgeApp.Models
+{
+    public class Ages
+    {
+        public DateTime? Birthdate { get; set; }
+        public string Age
+        {
+            get
+            {
+                string result = string.Empty;
+                if (Birthdate != null)
+                {
+                    //TimeSpan age = DateTime.Now.Subtract(Birthdate.Value);
+                    //int Totaldays = age.Days;
+                    //int years = Totaldays / 365;
+                    //Totaldays = Totaldays % 365;
+                    //int months = Totaldays / 30;
+                    //Totaldays = Totaldays % 30;
+
+                    DateTime current = DateTime.Now;
+                    int days = current.Day - Birthdate.Value.Day;
+                    if (days > 0)
+                        current = current.AddDays(-Birthdate.Value.Day);
+
+                    int months = current.Month - Birthdate.Value.Month;
+                    if (months > 0)
+                        current = current.AddMonths(-Birthdate.Value.Month);
+
+                    int years = current.Year - Birthdate.Value.Year;
+
+                    if (days < 0)
+                    {
+                        days = 31 + days;
+                        months-=1;
+                    }
+                    if (months < 0)
+                    {
+                        months = 12 + months;
+                        years -= 1;
+                    }
+
+                    result = string.Format("{0} years {1} months {2} days", years, months, days);
+                }
+                return result;
+            }
+        }
+    }
+}
